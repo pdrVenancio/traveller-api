f9042ab4ab5a7203fbfb451449f6ba4d0fc2f84d
pdrVenancio <pedrosantos121229@gmail.com>
2024-09-11 21:40:37 -0300

    first commit

--- app.js
+++ app.js
@@ -9,6 +9,10 @@ const app = express();
 app.use(express.json());
 app.use(cors())
 
+app.get("/", (req, res) => {
+    res.json({ message: "Hello World" });
+});
+
 app.use('/api', mainRouter)
 
 app.listen(3000, ()=> {
