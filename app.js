57f087c10b3241927691eff3c7779d1a7afcfbf3
pdrVenancio <pedrosantos121229@gmail.com>
2024-09-11 21:42:21 -0300

    first commit

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const cors = require("cors");
 const { mainRouter } = require("./routes/router");
 
-const port  = process.env.PORT || 3000
+const port  = process.env.PORT || 3001
 const app = express();
 
 app.use(express.json());
