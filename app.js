09611e3ba58c0d905bb689926ab58591d29ea70e
pdrVenancio <pedrosantos121229@gmail.com>
2024-09-11 21:53:48 -0300

    first commit

--- app.js
+++ app.js
@@ -1,20 +1,41 @@
-//importar libs externas
+// //importar libs externas
+// const express = require("express");
+// const cors = require("cors");
+// const { mainRouter } = require("./routes/router");
+
+// const port  = process.env.PORT || 3001
+// const app = express();
+
+// app.use(express.json());
+// app.use(cors())
+
+// app.get("/", (req, res) => {
+//     res.json({ message: "Hello World" });
+// });
+
+// app.use('/api', mainRouter)
+
+// app.listen(3000, ()=> {
+//     console.log("Server started! Port: 3000")
+// });
+
+// /api/app.js
 const express = require("express");
 const cors = require("cors");
-const { mainRouter } = require("./routes/router");
+const { mainRouter } = require("../routes/router"); // Ajuste o caminho se necessário
 
-const port  = process.env.PORT || 3001
 const app = express();
 
 app.use(express.json());
-app.use(cors())
+app.use(cors());
 
 app.get("/", (req, res) => {
     res.json({ message: "Hello World" });
 });
 
-app.use('/api', mainRouter)
+app.use('/api', mainRouter);
 
-app.listen(3000, ()=> {
-    console.log("Server started! Port: 3000")
-});
+// Exporta a função para o Vercel
+module.exports = (req, res) => {
+    app(req, res);
+};
