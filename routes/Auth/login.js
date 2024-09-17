11540f3b2b992dab8c063143603ffb57aebffc31
Pedro Venâncio dos Santos <89045459+pdrVenancio@users.noreply.github.com>
2024-09-17 12:28:16 -0300

    o que esta contencendo que o login nao enxerga usuarios add login.js

--- routes/Auth/login.js
+++ routes/Auth/login.js
@@ -9,23 +9,27 @@ require('dotenv').config();
 
 const bdPath = path.join(__dirname,'..', '..', 'db','banco-dados-usuario.json');
 const usuariosCadastrados = JSON.parse(fs.readFileSync(bdPath, {encoding: 'utf-8'}));
+console.log(usuariosCadastrados)
 
 loginRouter.post('/', async (req,res) => {
-
-    const {email, password} = req.body; 
-    for (let user of usuariosCadastrados){
-        if(user.email === email){
-            const passwordValidado = await bcrypt.compare(password, user.password);
-            if(passwordValidado===true){
-                const tokenAcesso = jwt.sign(user, 'dslbakdjbasldblshdbashdbashdjlsabdhjdblasdbjdbsflhbhwlebrewhjjkfkajdç', { expiresIn: '1h' });
-                return res.status(200).json(tokenAcesso);
-            }
-            else
-                return res.status(422).send(`Usuario ou senhas incorretas.`);
-        }   
+    try{
+        const {email, password} = req.body; 
+        for (let user of usuariosCadastrados){
+            if(user.email === email){
+                const passwordValidado = await bcrypt.compare(password, user.password);
+                if(passwordValidado===true){
+                    const tokenAcesso = jwt.sign(user, 'dslbakdjbasldblshdbashdbashdjlsabdhjdblasdbjdbsflhbhwlebrewhjjkfkajdç', { expiresIn: '1h' });
+                    return res.status(200).json(tokenAcesso);
+                }
+                else
+                    return res.status(422).send(`Usuario ou senhas incorretas.`);
+            }   
+        }
+        return res.status(409).send(`Usuario com email ${email} não existe!`);
+    }catch (error) {
+        console.error('Erro ao adicionar usuario:', error);
+        res.status(500).send('Erro interno do servidor.');
     }
-    return res.status(409).send(`Usuario com email ${email} não existe!`);
-
 });
 
 module.exports = {loginRouter}
