01896894fa8bc00c69f2236b2a54c1842244b85b
Pedro Venâncio dos Santos <89045459+pdrVenancio@users.noreply.github.com>
2024-09-17 11:46:11 -0300

    cors app.js

--- app.js
+++ app.js
@@ -1,32 +1,24 @@
-// //importar libs externas
 // const express = require("express");
 // const cors = require("cors");
-// const { mainRouter } = require("./routes/router");
+// const { mainRouter } = require("./routes/router"); // Ajuste o caminho se necessário
 
-// const port  = process.env.PORT || 3001
 // const app = express();
 
 // app.use(express.json());
-// app.use(cors())
+// app.use(cors());
 
 // app.get("/", (req, res) => {
 //     res.json({ message: "Hello World" });
 // });
 
-// app.use('/api', mainRouter)
+// app.use('/api', mainRouter);
 
-// app.listen(3000, ()=> {
-//     console.log("Server started! Port: 3000")
-// });
+// // Exporta a função para o Vercel
+// module.exports = (req, res) => {
+//     app(req, res);
+// };
 
-// /api/app.js
-// /api/index.js (ou use /app.js se não quiser renomear)
-const cors = require('cors');
- 
-// Permitir requisições de um domínio específico
-app.use(cors({
-    origin: 'https://main--subtle-klepon-ef900c.netlify.app'
-}));
+
 const express = require("express");
 const cors = require("cors");
 const { mainRouter } = require("./routes/router"); // Ajuste o caminho se necessário
@@ -34,7 +26,11 @@ const { mainRouter } = require("./routes/router"); // Ajuste o caminho se necess
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+
+// Permitir requisições de um domínio específico
+app.use(cors({
+    origin: 'https://main--subtle-klepon-ef900c.netlify.app'
+}));
 
 app.get("/", (req, res) => {
     res.json({ message: "Hello World" });
