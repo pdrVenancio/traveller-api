dd110916cf75390a370a22149871e30f8e3ef468
Pedro Venâncio dos Santos <89045459+pdrVenancio@users.noreply.github.com>
2024-09-17 11:27:21 -0300

    teste com try catch create.js

--- routes/Auth/create.js
+++ routes/Auth/create.js
@@ -10,32 +10,37 @@ const usuariosCadastrados = JSON.parse(fs.readFileSync(bdPath, {encoding: 'utf-8
 const createRouter = Router()
 
 createRouter.post('/',  async (req, res) => {
-    const {username, email, password} = req.body; 
-    let userIds = [];
-    for (let users of usuariosCadastrados){
-      if(users.email === email){
-          return res.status(409).send(`Usuario com email ${email} já existe.`);
-      }
-      userIds.push(users.id);
-    }
-
-    let id;
-    if(usuariosCadastrados.length > 0){
-      id = Math.max(...userIds) + 1;
-    }else{
-      id = 0;
-    }
-
-    const role = usuariosCadastrados.length > 0 ? 'user' : 'admin'; 
+    try{
+        const {username, email, password} = req.body; 
+        let userIds = [];
+        for (let users of usuariosCadastrados){
+          if(users.email === email){
+              return res.status(409).send(`Usuario com email ${email} já existe.`);
+          }
+          userIds.push(users.id);
+        }
     
-    const salt = await bcrypt.genSalt(10);
-    const passwordCrypt = await bcrypt.hash(password,salt);
-    const tickets = [];
-
-    const user = new User(id, username, role, email, passwordCrypt, tickets);
-
-    usuariosCadastrados.push(user);
-    fs.writeFileSync(bdPath,JSON.stringify(usuariosCadastrados,null,2));
-    res.status(201).send(`Usuario criado com sucesso!`);
+        let id;
+        if(usuariosCadastrados.length > 0){
+          id = Math.max(...userIds) + 1;
+        }else{
+          id = 0;
+        }
+    
+        const role = usuariosCadastrados.length > 0 ? 'user' : 'admin'; 
+        
+        const salt = await bcrypt.genSalt(10);
+        const passwordCrypt = await bcrypt.hash(password,salt);
+        const tickets = [];
+    
+        const user = new User(id, username, role, email, passwordCrypt, tickets);
+    
+        usuariosCadastrados.push(user);
+        fs.writeFileSync(bdPath,JSON.stringify(usuariosCadastrados,null,2));
+        res.status(201).send(`Usuario criado com sucesso!`);
+    }catch (error) {
+        console.error('Erro ao adicionar usuario:', error);
+        res.status(500).send('Erro interno do servidor.');
+    }
 })
 module.exports = { createRouter }
